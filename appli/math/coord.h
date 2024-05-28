1ac33415256c2de8bab1af7d1ae04432850c1749
Cyprien Ménard <cyprien.menard@reseau.eseo.fr>
2024-05-28 16:33:47 +0200

    Change angle range from (-pi - pi) to (0 - 2pi)

--- appli/math/coord.h
+++ appli/math/coord.h
@@ -3,17 +3,18 @@
 
 
 typedef int16_t coord_t;
-typedef int16_t angle_t;
+typedef uint16_t angle_t;
 
 /**
  * @brief Macro défissant la valeur de PI en entier
  * 0pi = 0
  * pi = 2^16/2
- * -pi = -2^16/2
  * En dehors de la plage, on a un overflow donc ça fait un modulo
  *
  */
 #define PI_INT ((1<<sizeof(angle_t))/2);
+
+#define QUANTUM 1000
 
 
 #endif
